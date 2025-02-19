1b61d0222e2d327b09a1c6caa2548283860e00b7
Nodirbek Rejabaliyev <n.rejabaliyev@newuu.uz>
2025-02-19 13:02:49 +0500

    Initial commit

---
+++ src/fonts.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import CurlyFont from '/src/fonts/curly.ttf';
+import LongFont from '/src/fonts/long.otf';
+
+const Fonts = () => {
+  useEffect(() => {
+    const style = document.createElement('style');
+    style.innerHTML = `
+      @font-face {
+        font-family: 'CurlyFont';
+        src: url(${CurlyFont}) format('truetype');
+        font-weight: normal;
+        font-style: normal;
+      }
+      
+      @font-face {
+        font-family: 'LongFont';
+        src: url(${LongFont}) format('opentype');
+        font-weight: normal;
+        font-style: normal;
+      }
+
+      
+    `;
+    document.head.appendChild(style);
+  }, []);
+
+  return null;
+};
+
+export default Fonts;

1b7b08a70cb3ccd4f85643d7e4da6df42d388419
dilnoza471 <d.eraliyeva@mewuu.uz>
2025-02-22 22:33:30 +0500

    buttons fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import SignUp from "./SignUp";
-
+import Menu from "./menu.jsx";
+import Rewards from "./rewards.jsx";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/menu"  element={<Menu />} />
+        <Route path='/rewards' element={<Rewards />} />
       </Routes>
     </Router>
   );
--- src/header.jsx
+++ src/header.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
-
 const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
         <a href="/">Coffee Shop</a>
         <nav className="nav-links">
-          <a href="#">About Us</a>
-          <a href="#reserve">Reserve</a>
-          <a href="#contact">Contact</a>
+          <Link to="/menu">Menu</Link>
+          <Link to='/rewards'>Rewards</Link>
+          <a href='#contact'>Contact</a>
           <Link to="/SignUp">Sign Up</Link>
+
         </nav>
       </div>
     </header>

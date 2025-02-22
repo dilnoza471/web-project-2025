c09950ac2c62332b4d44b8f418fc50a05abf2915
dilnoza471 <d.eraliyeva@mewuu.uz>
2025-02-23 00:15:14 +0500

    nav bar edit

--- src/header.jsx
+++ src/header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <a href="/">Coffee Shop</a>
         <nav className="nav-links">
+          <Link to="/">Coffee Shop</Link>
           <Link to="/menu">Menu</Link>
           <Link to='/rewards'>Rewards</Link>
           <a href='#contact'>Contact</a>
